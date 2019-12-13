cf8ec71cacac15ee4b0f97f9f6285e7208b610f0
lamp295367893 <295367893@qq.com>
2019-12-13 23:14:19 +0800

    更新变量混淆 优化include

--- phperpack.php
+++ phperpack.php
@@ -1,15 +1,79 @@
 <?php
-echo token_name(377);
-$entry = './index.php';
-new Handle($entry, './output.php');
+/**
+ * PHPpack使用说明
+ * -m 指定入口文件路径 可以是相当路径 也可以是绝对路径
+ * -o 指定输出文件位置 及文件名，可以是相对路径，也可以是绝对路径
+ * --v 开启变量名混淆
+**/
+new Main();
 
-class Handle {
+
+class Main {
+  public $entry;
+  public $outputPath;
+  static public $varConfusion = False;
+  public function __construct() {
+    $this->_init();
+  }
+
+  public function _init() {
+    global $argv;
+    $this->init_before();
+    $mainIndex = array_search('-m',$argv);
+    $mainIndex || die(new Message("对不起进程无法继续，请输入入口文件名称", 'error'));
+    $outputIndex = array_search('-o', $argv);
+    $this->outputPath = $outputIndex ? $this->handlePath($argv[ $outputIndex + 1 ]) : './output.php';
+    $mainFileName = $argv[$mainIndex + 1];
+    if (!$mainFileName) {
+      die(new Message('入口文件输入错误', 'error'));
+    }
+    if (array_search('--v', $argv)) {
+      Main::$varConfusion = True;
+    }
+
+    $this->entry = $this->handlePath($mainFileName);
+    new Handle($this->entry, $this->outputPath);
+  }
+
+  public function handlePath($path) {
+    if (strpos($path, '/') || strpos($path, '\\')) {
+      return $path;
+    }else{
+      return './' . $path;
+    }
+  }
+
+  public function init_before() {
+    echo new Message('-----------------------------------------------');
+    echo new Message('|------------------👏欢迎使用👏---------------|');
+    echo new Message('|------------------PHPpack 1.0----------------|');
+    echo new Message('|------------------作者：张宇童---------------|');
+    echo new Message('|--------------邮箱：admin@ecuuu.com----------|');
+    echo new Message('|-----------------PHPpack构建工具-------------|');
+    echo new Message('-----------------------------------------------');
+  }
+}
+
+
+class Handle extends Main {
   private $preprocessing; // 预处理文件内容
   private $afterContent; // 处理后的内容
   private $queue = []; // 语法队列
   private $queueName = [];
+  private $varMap = [];
+  private $keyWordSpace = [
+    'echo',
+    'function',
+    'class',
+    'private',
+    'public',
+    'protected',
+    'new',
+    'extends',
+  ];
 
   function __construct($filename, $outputPath) {
+
     $this->handle($filename);
     // $this->parse();
     $this->output($outputPath);
@@ -23,18 +87,26 @@ class Handle {
     }
     $this->queueName[] = $randomName;
     $this->queue[$randomName] = [];
+    echo new Message('加载：'.__DIR__.rtrim(ltrim($filePath,'\'.'),'\''));
     $content = file_get_contents(__DIR__.rtrim(ltrim($filePath,'\'.'),'\''));
     $temp = [];
-    var_dump(token_get_all($content));
     foreach (token_get_all($content) as $k => $v) {
       if (is_array($v)) {
         $temp_str = trim(trim($v[1]) == '<?php' ? '' : $v[1]);
         $temp_str = $temp_str == '?>'? '' : $temp_str;
         if ($temp_str != '') {
           if ($v[0] == 320) {
+            if (Main::$varConfusion) {
+              $temp_str = $this->varMap[$temp_str] = '$_'.md5($temp_str);
+            }
             array_push($temp, '\\'.$temp_str);
+          }else if($v[0] == 377) {
+
           }else {
             array_push($temp, $temp_str);
+          }
+          if ($v[0] == 319) {
+            array_push($temp, ' ');
           }
         }
       }else {
@@ -42,29 +114,36 @@ class Handle {
       }
     }
 
-
-    $jump = 0;
     $ret = [];
-    foreach($temp as $k => $v) {
-      if ($jump) {$jump--; continue;}
-      if ($v == 'include') {
-        if ($temp[$k+1] == '(') {
-          $temp_random = '_'.(string)mt_rand(1000000, 9999999);
-          array_push($this->queue[$randomName], " eval(\\\$$temp_random)");
-          $this->handle($temp[$k+2], $temp_random);
-          $jump = 3;
-        }else {
-          $temp_random = '_'.(string)mt_rand(1000000, 9999999);
-          array_push($this->queue[$randomName], " eval(\\\$$temp_random)");
-          $this->handle($temp[$k+1], $temp_random);
-          $jump = 1;
-        }
+    while(list($k, $v) = each($temp)) {
+      if ($v == 'include' || $v == 'include_once') {
+        $this->handle_parse_include($temp, $temp, $randomName);
       }else {
         array_push($this->queue[$randomName], $v);
       }
-      if ($v == 'echo') {
-        array_push($this->queue[$randomName],  ' ');
+      if (in_array($v, $this->keyWordSpace)) {
+        array_push($this->queue[$randomName], ' ');
       }
+    }
+  }
+
+  /**
+   * 处理include
+  **/
+  function handle_parse_include($arr, &$origin_arr, $randomName) {
+    list($k, $v) = each($arr);
+    if ($v == '(') {
+      $temp_random = '_'.(string)mt_rand(1000000, 9999999);
+      array_push($this->queue[$randomName], " eval(\\\$$temp_random)");
+      $this->handle(each($arr)['value'], $temp_random);
+      for ($i = 0; $i < 3; $i++) {
+        each($origin_arr);
+      }
+    }else {
+      $temp_random = '_'.(string)mt_rand(1000000, 9999999);
+      array_push($this->queue[$randomName], " eval(\\\$$temp_random)");
+      $this->handle($v, $temp_random);
+      each($origin_arr);
     }
   }
   /**
@@ -84,6 +163,7 @@ class Handle {
     $reverse_name = array_reverse($reverse_name);
     $output_str .= "eval(\$$reverse_name[0]);";
     file_put_contents($outputPath, $output_str);
+    echo new Message("👌PHPpack为您构建成功！\r\n🚗构建后文件位置：{$outputPath}", 'success');
   }
   /**
    * xxx
@@ -94,4 +174,28 @@ class Handle {
     $array = array_merge ($first_array, $insert_array, $array);
   }
   
+}
+
+class Message {
+  public function __construct($message, $type = 'info') {
+    $this->case_type($type, $message);
+  }
+
+  public function case_type ($type, $message) {
+    switch ($type) {
+      case 'error':
+        $this->message = "\033[31m{$message}\033[0m\r\n";
+        break;
+      case 'success':
+        $this->message = "\033[32m{$message}\033[0m\r\n";
+        break;
+      case 'info':
+        $this->message = "\033[36m{$message}\033[0m\r\n";
+        break;
+    }
+  }
+
+  public function __toString() {
+    return $this->message;
+  }
 }
